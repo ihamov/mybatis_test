de3070c3771af8eb44e00d3428e11da6330033d8
Vigo <ihamov@163.com>
2017-09-20 22:48:45 +0800

    缓存相关知识

--- mybatistest1/src/main/java/com/du/mybatis/bean/Employee.java
+++ mybatistest1/src/main/java/com/du/mybatis/bean/Employee.java
@@ -1,6 +1,12 @@
 package com.du.mybatis.bean;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String lastName;
     private String gender;
